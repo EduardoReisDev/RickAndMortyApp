1fbb8ae0b4b1413085ea2523b4f2c9bc2cdacce9
Eduardo Reis <eduardoreisdev@gmail.com>
2022-03-17 21:25:41 -0300

    WIP

---
+++ RickAndMorty/RickAndMorty/Constants/APIEndpoints.cs
@@ -0,0 +1,8 @@
+﻿using System;
+namespace RickAndMorty.Constants
+{
+    public static class APIEndpoints
+    {
+        public static string GraphQLUri = "https://rickandmortyapi.com/graphql";
+    }
+}
