457108acb32699c2402104505a67a6f3de07e1ed
Eduardo Reis <eduardoreisdev@gmail.com>
2022-03-17 22:23:26 -0300

    WIP

---
+++ RickAndMorty/RickAndMorty/Models/Character.cs
@@ -0,0 +1,16 @@
+﻿using System;
+namespace RickAndMorty.Models
+{
+    public class Character
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Status { get; set; }
+        public string Species { get; set; }
+        public string Type { get; set; }
+        public string Gender { get; set; }
+        public string Image { get; set; }
+        public Location Origin { get; set; }
+        public Location Location { get; set; }
+    }
+}
---
+++ RickAndMorty/RickAndMorty/Models/Location.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace RickAndMorty.Models
+{
+    public class Location
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Type { get; set; }
+    }
+}
