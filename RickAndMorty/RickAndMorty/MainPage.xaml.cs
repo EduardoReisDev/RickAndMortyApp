63ebf0b8b35e3d437ec806ebf5feea7d387f4163
Eduardo Reis <eduardoreisdev@gmail.com>
2022-03-17 23:49:01 -0300

    WIP

--- RickAndMorty/RickAndMorty/MainPage.xaml.cs
+++ RickAndMorty/RickAndMorty/MainPage.xaml.cs
@@ -6,6 +6,7 @@ using System.Text;
 using System.Threading.Tasks;
 using GraphQL;
 using GraphQL.Client.Abstractions;
+using GraphQL.Client.Http;
 using RickAndMorty.Models;
 using Xamarin.Forms;
 
@@ -13,7 +14,7 @@ namespace RickAndMorty
 {
     public partial class MainPage : ContentPage
     {
-        private readonly IGraphQLClient client;
+        private GraphQLHttpClient client;
 
         public MainPage()
         {
@@ -24,6 +25,8 @@ namespace RickAndMorty
 
         public async void Teste()
         {
+            client = new GraphQLHttpClient();
+
             var query = new GraphQLRequest
             {
                 Query = @"query {
